58be667a6e0653ab79ca71da58b78b85617684a4
Simone Gaiarin <simgunz@gmail.com>
2015-11-24 13:33:27 +0100

    Fix script to build deck to make it compatible with OSX

--- build-deck.sh
+++ build-deck.sh
@@ -2,6 +2,6 @@
 
 cd deck
 cat collection-dump.sql | sqlite3 collection.anki2
-find  -type f ! -name "collection-dump.sql" | xargs zip "../Common IPA.apkg"
+find ./ -type f ! -name "collection-dump.sql" | xargs zip -j "../Common IPA.apkg"
 rm collection.anki2
 cd ..
